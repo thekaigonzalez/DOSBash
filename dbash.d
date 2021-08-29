4a0d3a42aebf9b11f2509dedde213b28952a3426
Kai D. Gonzalez <gkai70264@gmail.com>
2021-08-29 17:07:05 -0400

    a'

--- dbash.d
+++ dbash.d
@@ -1,11 +1,11 @@
 import std.stdio;
 import std.process;
-
+import std.file;
 void main()
 {
  while (true)
  {
-  write("C:\\>");
+  write("C:\\"~getcwd()~">");
   char[] op;
   readln(op);
   writeln(executeShell(op).output);
